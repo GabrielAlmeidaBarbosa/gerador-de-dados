af3c11f3b2fe25a8355fa5134e4c5aa26432e8e0
Gabriel Almeida Barbosa <gabrielalmbarbosa@gmail.com>
2022-02-15 14:06:17 -0300

    Documentando as funçcionalidades com docstrings

--- lib/__init__.py
+++ lib/__init__.py
@@ -5,16 +5,30 @@ from random import choice
 
 # Funções de texto
 def line():
+    """
+    -> Cria uma linha tracejada.
+    :return: (str) linha tracejada.
+    """
     return '-' * 60
 
 
 def title(message):
+    """
+    -> Mostra um cabeçalho na tela com uma mensagem.
+    :message: Mensagem que será mostrada no cabeçalho
+    :return: sem retorno.
+    """
     print(line())
     print(f'|{message:^58}|')
     print(line())
 
 
 def menu(*options):
+    """
+    -> Mostra na tela um menu de opções para o usuário.
+    :*options: (str) uma ou mais opções que o menu vai conter.
+    :return: sem retorno.
+    """
     for index, option in enumerate(options):
         print(f'[ \033[0;33m{index + 1}\033[m ] - \033[0;33m{option}\033[m')
     print()
@@ -24,6 +38,11 @@ def menu(*options):
 
 # Funções de leitura de entrada do usuário
 def read_input_option(message):
+    """
+    -> Faz a leitura da opção/das opções escolhidas pelo usuário.
+    :message: (str) mensagem que será mostrada para o usuário.
+    :return: (bool) False or dict(...)
+    """
     while True:
         user_option = str(input(message)).strip().lower()
         if user_option == 'parar':
@@ -35,6 +54,12 @@ def read_input_option(message):
 
 
 def read_input_save(message, data):
+    """
+    -> Mostra na tela os dados gerados e salva ou não esses dados de acordo com a escolha do usuário.
+    :message: (str) mensagem que será mostrada para o usuário
+    :data: (dict) dados que foram gerados
+    :return: sem retorno.
+    """
     for k, v in data.items():
         sleep(1)
         print(f'{k}: {v}')
@@ -66,6 +91,11 @@ def read_input_save(message, data):
 
 
 def validate_user_input_options(inp):
+    """
+    -> Faz a validação da entrada de dados do usuário.
+    :inp: (str) informada pelo usuário
+    :return: (bool) False | (dict) data
+    """
     inp_options = inp.split(',')
     for op in inp_options:
         if len(op) > 1 or len(op) < 1:
@@ -95,6 +125,11 @@ def validate_user_input_options(inp):
 
 # Salvar dados gerados
 def save_data(data):
+    """
+    -> Salva os dados gerados em um arquivo de texto
+    :data: (dict) dados gerados
+    :return: (bool) True | False
+    """
     try:
         archive = open('data.txt', 'a')
         archive.write(f'{data}\n')
@@ -106,6 +141,11 @@ def save_data(data):
 
 # Função de alerta de opção inválida.
 def invalid_option(option=''):
+    """
+    -> Mostra uma mensagem informando que a opção digitada é inválida.
+    :option: (opcional) (str) entrada informada pelo usuário.
+    :return: sem retorno.
+    """
     if option == '':
         print('\033[0;31mOpção inválida! Tente novamente...\033[m')
     else:
